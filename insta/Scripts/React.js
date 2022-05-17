7fe313a899f280b9aee193a389b38a5a91395234
salmagalal <salmaahmedgalal@gmail.com>
2022-05-17 20:01:40 +0200

    Project initialization

---
+++ insta/Scripts/React.js
@@ -0,0 +1,20 @@
+﻿
+function sendReact(id,idstu,react) {
+    var x = document.getElementById(id);
+    var fa = new FormData();
+    fa.append("idpost", x.id);
+    fa.append("react", react);
+    fa.append("iduser", idstu)
+    $.ajax({
+        url: 'http://localhost:51338/api/React',
+        type: 'POST',
+        data: fa,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            console.log(data);
+            x.value = "";
+        }
+    });
+}
---
+++ insta/Scripts/comment.js
@@ -0,0 +1,22 @@
+﻿function sendcomment(id, idstu) {
+    var x = document.getElementById(id);
+    var fa = new FormData();
+
+    fa.append("idpost", x.id);
+    fa.append("comment", x.value);
+    fa.append("iduser", idstu)
+    
+
+    $.ajax({
+        url: 'http://localhost:51338/api/Comment',
+        type: 'POST',
+        data: fa,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            console.log(data);
+            x.value = "";
+        }
+    });
+}
