e4718731e163d371dc0960ee7a25aa451c45a03c
salmagalal <salmaahmedgalal@gmail.com>
2022-05-17 20:50:41 +0200

    Database

---
+++ insta/Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace insta.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [Display(Name = " Add Comment")]
+        public string CommentText { get; set; }
+
+
+        // [ForeignKey("User")]
+        // public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual Post Post { get; set; }
+    }
+}
---
+++ insta/Models/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace insta.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "the string length must be 50")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "the string length must be 50")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        internal User LogIn(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Mobile Number")]
+        public string MobileNumber { get; set; }
+
+        [FileExtensions(Extensions = "jpg,jpeg,png")]
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Profile Photo")]
+        public string Photo { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+
+       
+        public virtual List<Post> Posts { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Reacts> Reacts { get; set; }
+
+    }
+}
