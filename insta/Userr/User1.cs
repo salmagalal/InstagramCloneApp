582cb924c4bc586cd400a7b9ab34245589085771
salmagalal <salmaahmedgalal@gmail.com>
2022-05-17 20:24:54 +0200

    User

---
+++ insta/Userr/User1.cs
@@ -0,0 +1,31 @@
+﻿using insta.Context;
+using insta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace insta.Userr
+{
+    public class User1
+    {
+        private DataContext db = new DataContext();
+        public User user = new User();
+
+        public User Login(string Username, string Password)
+        {
+            if (Username == "" || Password == "")
+            {
+                return null;
+            }
+            user = db.User.Where(x => x.Username == Username && x.Password == Password).FirstOrDefault();
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
+        }
+
+    }
+}
