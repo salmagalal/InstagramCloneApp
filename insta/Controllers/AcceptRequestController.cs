29cdc4eb7f015810ca113409a5f9b1e5e98dc6d4
ZiadMamdouh <102364735+ZiadMamdouh@users.noreply.github.com>
2022-05-17 22:08:45 +0200

    Accept and Reject and View Profile

---
+++ insta/Controllers/AcceptRequestController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using insta.Context;
+using insta.Models;
+
+namespace insta.Controllers
+{
+    public class AcceptRequestController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        public ActionResult Accept(int? id)
+        {
+            int SenderId = Convert.ToInt32(Session["Userid"]);
+            int RecieverId = Convert.ToInt32(id);
+
+            try
+            {
+                db.Friend.RemoveRange(db.Friend.Where(m => m.Sender_Id== SenderId && m.Reciever_Id == RecieverId).ToList());
+                db.SaveChanges();
+               
+            }
+            catch
+            {
+
+            }
+
+
+
+            Friend friend = new Friend();
+
+            friend.Sender_Id = SenderId;
+            friend.Sender= db.User.Find(SenderId);
+
+
+            friend.Reciever_Id = RecieverId;
+            friend.Reciever = db.User.Find(RecieverId);
+
+            db.Friend.Add(friend);
+            db.SaveChanges();
+
+
+            Friend friend2 = new Friend();
+
+            friend2.Sender_Id = RecieverId;
+            friend2.Sender = db.User.Find(RecieverId);
+
+        
+            friend2.Reciever_Id = SenderId;
+            friend2.Reciever = db.User.Find(SenderId);
+
+
+            db.Friend.Add(friend2);
+            db.SaveChanges();
+
+
+
+            db.FriendRequest.RemoveRange(db.FriendRequest.Where(m => m.Sender_Id == RecieverId && m.Reciever_Id == SenderId).ToList());
+            db.SaveChanges();
+            return RedirectToAction("IncomingRequests","FriendRequests");
+        }
+
+
+    }
+}
---
+++ insta/Controllers/RejectController.cs
@@ -0,0 +1,87 @@
+﻿using insta.Context;
+using insta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace insta.Controllers
+{
+    public class RejectController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        public ActionResult Reject(int? id)
+        {
+
+
+
+            int SenderId = Convert.ToInt32(Session["Userid"]);
+            int RecieveId = Convert.ToInt32(id);
+
+
+
+            try
+            {
+                db.Friend.RemoveRange(
+                db.Friend.Where(m => m.Sender_Id == SenderId && m.Reciever_Id == RecieveId).ToList());
+                db.SaveChanges();
+
+
+
+                db.Friend.RemoveRange(db.Friend.Where(m => m.Sender_Id == RecieveId && m.Reciever_Id == SenderId).ToList());
+                db.SaveChanges();
+            }
+            catch
+            {
+            }
+
+
+
+            Friend friend = new Friend();
+
+
+
+            friend.Sender_Id = SenderId;
+            friend.Sender = db.User.Find(SenderId);
+
+
+
+            friend.Reciever_Id = RecieveId;
+            friend.Reciever = db.User.Find(RecieveId);
+
+
+
+            
+
+
+
+            db.SaveChanges();
+
+
+
+            friend.Reciever_Id= SenderId;
+            friend.Reciever = db.User.Find(SenderId);
+
+
+
+            friend.Sender_Id = RecieveId;
+            friend.Sender = db.User.Find(RecieveId);
+
+
+
+            
+
+
+
+            db.SaveChanges();
+
+
+
+            db.FriendRequest.RemoveRange(db.FriendRequest.Where(m => m.Reciever_Id == SenderId && m.Sender_Id == RecieveId).ToList());
+            db.SaveChanges();
+            return RedirectToAction("IncomingRequests", "FriendRequests");
+        }
+    }
+}
