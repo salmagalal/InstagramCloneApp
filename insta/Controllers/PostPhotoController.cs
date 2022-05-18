b07feb9b4ce9b794e11cee8c2172d9ca264ac7d9
salmaosman <salmaosman1520012010@gmail.com>
2022-05-18 06:55:09 +0200

    Add Post Clean Code

--- insta/Controllers/PostPhotoController.cs
+++ insta/Controllers/PostPhotoController.cs
@@ -14,46 +14,18 @@ namespace insta.Controllers
         private DataContext db = new DataContext();
 
         // GET: PostPhoto
-        public ActionResult Index()
+        public ActionResult PostPhoto()
         {
             if (Session["Userid"] == "0" && Session["Userid"] == null)
             {
                 return RedirectToAction("User", "LogIn");
             }
 
-            User u = db.User.Find(Convert.ToInt32(Session["Userid"]));
-            return View(u);
+            User userID = db.User.Find(Convert.ToInt32(Session["Userid"]));
+            return View(userID);
         }
 
 
-        [HttpGet]
-        public ActionResult AddPost()
-        {
-            if (Session["Userid"] == "0" && Session["Userid"] == null)
-            {
-                return RedirectToAction("LogIn", "User");
-            }
-
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult AddPost(HttpPostedFileBase photo)
-        {
-            HttpPostedFileBase postedFile = Request.Files["photo"];
-            string path = Server.MapPath("~/images/posts/");
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-            postedFile.SaveAs(path + Path.GetFileName(postedFile.FileName));
-            Post post = new Post();
-            post.Photo = "/images/posts/" + Path.GetFileName(postedFile.FileName);
-            post.User = db.User.Find(Convert.ToInt32(Session["Userid"]));
-            db.Post.Add(post);
-            db.SaveChanges();
-            return RedirectToAction("Index","PostPhoto");
-        }
-
+        
     }
 }
