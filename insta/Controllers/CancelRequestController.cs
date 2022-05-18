1779c29e5e5b6c5da395b60af964c9a1ab213c56
sara-ali5 <asara7504@gmail.com>
2022-05-18 05:56:07 +0200

    test

---
+++ insta/Controllers/CancelRequestController.cs
@@ -0,0 +1,34 @@
+﻿using insta.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using insta.Models;
+
+namespace insta.Controllers
+{
+    public class CancelRequestController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        
+       public ActionResult Cancel(int? id)
+        {
+            int SenderId= Convert.ToInt32(Session["Userid"]);
+            int RecieveId = Convert.ToInt32(id);
+            try
+            {
+                List<FriendRequest> FriendRequests = db.FriendRequest.Where( m => m.Sender_Id== SenderId && m.Reciever_Id == RecieveId ).ToList();
+                db.FriendRequest.RemoveRange(FriendRequests);
+                db.SaveChanges();
+            }
+            catch
+            {
+            }
+            return RedirectToAction("MyRequests","MyRequests");
+        }
+
+
+    }
+}
---
+++ insta/Migrations/202205180249151_clean.cs
@@ -0,0 +1,16 @@
+﻿namespace insta.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class clean : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
